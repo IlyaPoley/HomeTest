19f784672450332c2962867923356b2608372896
Ilya <52371671+IlyaPoley@users.noreply.github.com>
2020-07-19 23:46:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+/* открытие модального окна */
+$('[data-modal=area]').on('click', function() {
+    $('.modal, #area').fadeIn('slow');
+});
+
+/* закрытие модального окна */
+$('.modal__close').on('click', function() {
+    $('.modal, #area').fadeOut('slow');
+});
+
+$('.btn').on('click', function(e) {
+    e.preventDefault();
+    $('.box').toggleClass('box_active');
+    $('.btn').toggleClass('btn_active');
+  });
